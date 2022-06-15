6e5486a54968b65646d7e3fd0d3a57c5eb525a1b
guocong <guocong@haier.com>
2022-06-15 16:00:47 +0800

    init

---
+++ assets/js/10.bad8c0bc.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{786:function(n,e,t){"use strict";t.r(e);var l=t(81),r=Object(l.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("\r\n\r\n  hello world\r\n")])}),[],!1,null,null,null);e.default=r.exports}}]);
